1d8d313c700061027dc0981f75fbdbc3cb50a61d
CodingRuo <r.schober@outlook.com>
2022-05-03 19:45:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import 'assets/style.css';
+</style>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Welcome to the Forum</h1>
+  <ThreadList :threads="threads" />
+</template>
+
+<script>
+import srcData from '../data.json';
+import ThreadList from '../components/ThreadList.vue';
+
+export default {
+  name: 'PageHome',
+  components: {
+    ThreadList,
+  },
+  data() {
+    return {
+      threads: srcData.threads,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
